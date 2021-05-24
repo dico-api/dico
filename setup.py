a12ea57f2e5cbb8410a106718121cc12b1246165
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-05-24 14:55:43 +0900

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="dico-api",
+    version="0.0.1",
+    author="eunwoo1104",
+    author_email="sions04@naver.com",
+    description="Yet another Discord API wrapper for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eunwoo1104/dico",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=["aiohttp", "requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
