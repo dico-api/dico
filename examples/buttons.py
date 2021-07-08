21195e76c801f1ef46bd4a95e77847b9b0220f04
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-07-08 09:20:25 +0900

    Added more examples

---
+++ examples/buttons.py
@@ -0,0 +1,26 @@
+import dico
+
+client = dico.Client("YOUR_BOT_TOKEN")
+
+client.on_ready = lambda ready: print(f"Bot ready, with {len(ready.guilds)} guilds.")
+
+
+@client.on_message_create
+async def on_message_create(message: dico.Message):
+    if message.content.startswith("!button"):
+        button = dico.Button(style=dico.ButtonStyles.PRIMARY, label="Hello!", custom_id="hello")
+        button2 = dico.Button(style=dico.ButtonStyles.DANGER, label="Bye!", custom_id="bye")
+        row = dico.ActionRow(button, button2)
+        await message.reply("Button!", component=row)
+
+
+@client.on_interaction_create
+async def on_button_response(interaction: dico.Interaction):
+    if not interaction.type.message_component or not interaction.data.component_type.button:
+        return
+    resp = dico.InteractionResponse(callback_type=dico.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+                                    data=dico.InteractionApplicationCommandCallbackData(content=f"Yes, it's {interaction.data.custom_id}.",
+                                                                                        flags=dico.InteractionApplicationCommandCallbackDataFlags.EPHEMERAL))
+    await interaction.create_response(resp)
+
+client.run()
---
+++ examples/select.py
@@ -0,0 +1,31 @@
+import dico
+
+client = dico.Client("YOUR_BOT_TOKEN")
+
+client.on_ready = lambda ready: print(f"Bot ready, with {len(ready.guilds)} guilds.")
+
+
+@client.on_message_create
+async def on_message_create(message: dico.Message):
+    if message.content.startswith("!select"):
+        options = [
+            dico.SelectOptions(label="Label 1", value="l1"),
+            dico.SelectOptions(label="Label 2", value="l2"),
+            dico.SelectOptions(label="Label 3", value="l3")
+        ]
+        select = dico.SelectMenu(custom_id=f"select{message.guild_id}{message.id}{message.author.id}",
+                                 options=options)
+        row = dico.ActionRow(select)
+        await message.reply("Select!", component=row)
+
+
+@client.on_interaction_create
+async def on_select_response(interaction: dico.Interaction):
+    if not interaction.type.message_component or not interaction.data.component_type.select_menu:
+        return
+    resp = dico.InteractionResponse(callback_type=dico.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+                                    data=dico.InteractionApplicationCommandCallbackData(content=f"You selected {interaction.data.values[0]}.",
+                                                                                        flags=dico.InteractionApplicationCommandCallbackDataFlags.EPHEMERAL))
+    await interaction.create_response(resp)
+
+client.run()
