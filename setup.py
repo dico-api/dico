ed316a240201fa72a931992e5ebf77498a4a9180
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-12-08 12:59:46 +0900

    Fixed binary files not included

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dico-api/dico",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     python_requires='>=3.7',
     install_requires=["aiohttp", "requests"],
     classifiers=[
