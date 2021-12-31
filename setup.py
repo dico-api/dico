2369d30b6394a0059e835df05a99f5e15f9242db
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-12-31 15:46:36 +0900

    Version Up

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ import setuptools
 with open("README.md", "r", encoding="UTF-8") as f:
     long_description = f.read()
 
+with open("requirements.txt", "r", encoding="UTF-8") as f:
+    install_requires = f.read().splitlines()
+
+with open("requirements-dev.txt", "r", encoding="UTF-8") as f:
+    dev_requires = f.read().splitlines()
+
 setuptools.setup(
     name="dico-api",
-    version="0.0.29",
+    version="0.0.30",
     author="eunwoo1104",
     author_email="sions04@naver.com",
     description="Yet another Discord API wrapper for Python.",
@@ -15,9 +21,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires='>=3.7',
-    install_requires=["aiohttp", "requests"],
+    install_requires=install_requires,
     extras_require={
-        "voice": ["PyNaCl"]
+        "voice": ["PyNaCl"],
+        "dev": dev_requires
     },
     classifiers=[
         "Programming Language :: Python :: 3"
