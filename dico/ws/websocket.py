8285f4788af097955b6f0ccdc357ba9061e0870e
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2022-03-01 16:33:24 +0900

    Fixed v10 is not enforced on mono sharded

--- dico/ws/websocket.py
+++ dico/ws/websocket.py
@@ -415,7 +415,7 @@ class WebSocketClient:
         presence: typing.Optional[dict] = None,
     ):
         extra = "compress=zlib-stream" if compress else ""
-        base_url = gw_response.url + f"?v=9&encoding=json" + extra
+        base_url = gw_response.url + f"?v=10&encoding=json" + extra
         ws = await http.session.ws_connect(base_url, **cls.WS_KWARGS)
         return cls(
             http,
